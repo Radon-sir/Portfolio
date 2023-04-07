8e1b4de04f7245c181584d897d98b276dd74c64d
Rahul Naval <118505793+Radon-sir@users.noreply.github.com>
2023-04-07 22:10:25 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,9 @@ import logo from "../Assets/RAhul_Naval-2-removebg-preview.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
 import {
   AiFillStar,
   AiOutlineHome,
-  AiOutlineFundProjectionScreen,
   AiOutlineUser,
 } from "react-icons/ai";
 
